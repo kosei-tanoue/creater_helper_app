becaea2c7fb6d6e08ed625ff46ece4c8d53024b3
kosei-tanoue <korxi.music.glitch.complex@gmail.com>
2021-02-25 22:41:31 +0900

    検索機能の触り

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :requests do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users
-  # root 'messages#index'
+
   
 
   resources :rooms do
